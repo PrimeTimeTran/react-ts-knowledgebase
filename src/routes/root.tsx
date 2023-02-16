a1f94ae7afbf8a8216689ae57029b23cac77b5fa
Loi Van Tran <datloiboi@gmail.com>
2023-02-15 23:35:19 -0500

    Add tree and icon

--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -3,11 +3,30 @@ import {
   Outlet,
 } from "react-router-dom";
 
-import { Sidebar, Menu, MenuItem, SubMenu } from 'react-pro-sidebar';
+import { Sidebar, Menu, MenuItem, SubMenu, menuClasses } from 'react-pro-sidebar';
 
 import { Row, Col } from 'react-bootstrap';
 
 import Nav from '../components/Navbar'
+
+export interface IconProps extends React.SVGAttributes<HTMLOrSVGElement> {
+  size?: number;
+}
+// https://fontawesomeicons.com/svg/icons/layout-sidebar-reverse
+export const BarChart: React.FC<IconProps> = ({ size = 18, ...rest }) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width={size}
+      height={size}
+      viewBox="0 0 24 24"
+      fill="currentColor"
+      {...rest}
+    >
+      <path d="M16 3a2 2 0 0 0-2-2H2a2 2 0 0 0-2 2v10a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V3zm-5-1v12H2a1 1 0 0 1-1-1V3a1 1 0 0 1 1-1h9zm1 0h2a1 1 0 0 1 1 1v10a1 1 0 0 1-1 1h-2V2z"></path>
+    </svg>
+  );
+};
 
 export default function Root() {
   return (
@@ -31,12 +50,28 @@ export default function Root() {
               <button type="submit">New</button>
             </form>
           </div>
-          <Sidebar>
+          <Sidebar
+            rootStyles={{
+              [`.${menuClasses.label}`]: {
+                overflowX: 'scroll',
+                textOverflow: 'clip',
+              },
+              [`.${menuClasses.label}::-webkit-scrollbar`]: {
+                display: 'none',
+              },
+              [`.${menuClasses.icon}`]: {
+
+              }
+            }}
+          >
             <Menu>
-              <MenuItem component={<Link to={`posts/2PACX-1vRw4Y8-rAKlBgO4_KgfnHa4tV8I0xT-26c9Erf7zXKl-_G3nRjBTTzy5eDtOe32txP4-LfMMNNESSis`} />}>
+              <MenuItem icon={<BarChart />}
+                component={<Link to={`posts/2PACX-1vRw4Y8-rAKlBgO4_KgfnHa4tV8I0xT-26c9Erf7zXKl-_G3nRjBTTzy5eDtOe32txP4-LfMMNNESSis`} />}>
                 MVP
               </MenuItem>
-              <SubMenu label="Charts">
+              <SubMenu
+                label="Charts"
+              >
                 <MenuItem component={<Link to={`posts/2PACX-1vRbQsv2Jt8kx0VEDz8DODDHiSmMw_jRwXE3yksdgRoCUn-S0GQBuzyyttCj9h4m5vdQh2q0t7-ae_f4`} />}>
                   Product Market Fit
                 </MenuItem>
