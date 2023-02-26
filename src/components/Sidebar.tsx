e8628826a93d3a5b7981221ed194efc44b337cb2
Loi Van Tran <datloiboi@gmail.com>
2023-02-26 15:48:22 -0500

    WIP

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,86 +1,23 @@
 import { useState } from 'react'
 import {
-  Link,
   useNavigate
 } from "react-router-dom";
 
-import { SubMenu, Sidebar, Menu, MenuItem, sidebarClasses, menuClasses } from 'react-pro-sidebar';
+import { CaretOpen, CaretRight } from './Caret'
 
-import IconLeft, { CaretOpen, CaretRight } from './Caret'
-
-const items = [
-  {
-    label: 'Những thứ cơ bản',
-    url: '',
-    submenu: [
-      {
-        label: 'Khởi nghiệp là gì',
-        url: '2PACX-1vQGgH5HglrtN4H6m1W_0Z35TNpdEBNuGxLaSY_CfN9fnFMV4XbUgVhCFzAxt6IEoAPetUgLX8ZbktZx',
-      },
-      {
-        label: 'Các giai đoạn khởi nghiệp',
-        url: '2PACX-1vTdE4TjDe2T9nu4TPGBf4Zz9uSbFewv0MzSLsW86PsOBWN1O0Ho8RrIKEhkv4VcGnJBF2qWDI25mU0k',
-      },
-      {
-        label: 'Tham gia một công ty khởi nghiệp',
-        url: '2PACX-1vS3KLyYbO5XMyRFiQNGtztww7Qp8LggbjrCEiMex6h2oxT2XxDrkiUK7Wk3gJqx3k3R4JYuEIiZyi_h',
-      },
-      {
-        label: 'Tại sao nên làm việc tại một công ty khởi nghiệp',
-        url: '2PACX-1vR2-lurxR7npZ_qKp3ZjwLeNH4Pxemq9QRfUxfkoxiTyOL5g2BQbuZJaLeeLCafnO13N8mahXVYRTn6',
-      },
-      {
-        label: 'Tại sao không nên làm việc tại một công ty khởi nghiệp',
-        url: '2PACX-1vQClV-Ohy0oBTqL9lfsMVppUc8G1PvqWMMjsPnHsuakjUL5-XK8yS0SwKXZkXgADxBR-wJU-dRHhBb6',
-      },
-      {
-        label: 'Viết một sơ yếu lý lịch tuyệt vời',
-        url: '2PACX-1vQoOnwOMVGkMZ21cOvNwZUcnVJ8tMPvsG1oNZq3S4JCgvBVsMWwP8t6AkniFAb8HvUc49UJ4ccFmsB8',
-      },
-    ]
-  },
-  {
-    label: 'Lời khuyên khởi nghiệp',
-    url: '',
-    submenu: [
-      {
-        label: 'Tạo ra thứ gì đó mà mọi người yêu thích',
-        url: '2PACX-1vSbtgVLsGMWZlnYt2syAmFLeSSLzXes-4aJOZnYxrYCW8p19-XCLwbUYulCCBAjm1tcbVUGqB27SOEZ',
-      },
-      {
-        label: 'Làm những việc không mở rộng quy mô',
-        url: '2PACX-1vQQnr_xn8doZtiZnFLkk-kStIbQLu7EXylC5_eZRFo2LA4Y-d-nIn_6jxGsgyATElv6_9UJDTDQiBhR',
-      },
-      {
-        label: 'Suy thoái kinh tế',
-        url: '2PACX-1vQIyPlYTQdwGxWh-G96_LkhVKeibe9VwJq8XG2Dn27CahLNL-iRqX-uJSCa6K0dTnyDtrW7Ac-O31m3',
-      },
-      {
-        label: 'Không ngừng chăm chỉ',
-        url: '2PACX-1vTwpqNR2nI12ULj9jzHfKeMfaRwuXhD8aZZ-PGSo0Bt6V0dJjDrrgjK1_PmgYeh0d-wafHL-jfCsftX',
-      },
-      {
-        label: 'Công ty khởi nghiệp phát triển nhanh chóng',
-        url: '2PACX-1vQKlGhuwHQlZ5zT41IzE4SF7SUFDseY5zPLbXC2CFLbPLtYvv6im4IBNHJ74La22ZPJwlcOk-Cx7o45',
-      },
-      {
-        label: 'Khởi nghiệp = Tăng trưởng',
-        url: '2PACX-1vTjrPn6szVpKKNloxqG8OnQokG7i1bMzUDmQz82VbY6B3QJY1bMNwfC0MGy8TeNfYyKuq0sh6KvaDL4',
-      },
-      {
-        label: '90/10 Giải pháp',
-        url: '2PACX-1vT3xcjBHOd0UuUPAT00ALl8-51KeWOWHRrOtYnalsXgtwnD3Yt5DQFKhKY8yV6pP44OcFOSt9NqZqve',
-      },
-    ]
-  }
-]
+// @ts-ignore
+import { items } from '../utils'
 
 type SubmenuProps = {
   label: string;
   url: string;
   submenu: {
     label: string;
-    url: string
+    url: string;
+    submenu?: {
+      label: string;
+      url: string
+    }[];
   }[];
 }
 
@@ -111,16 +48,26 @@ function Submenu(props: { i: SubmenuProps }) {
           </ul>
         </li>
       </ul>}
+      {open && <ul className="submenu">
+        {props.i.submenu && props.i.submenu.map(s => <li onClick={() => navigate(`posts/${s.url}`)}>{s.label}</li>)}
+        <li>
+          <ul className="submenu-item">
+            {items.map((i: SubmenuProps) => (
+              <Submenu i={i} key={i.label} />
+            ))}
+          </ul>
+        </li>
+      </ul>}
     </li>
   )
 }
 
 export default function Aside() {
   return (
-      <ul>
-        {items.map((i: SubmenuProps) => (
-          <Submenu i={i} key={i.label} />
-        ))}
+    <ul>
+      {items.map((i: SubmenuProps) => (
+        <Submenu i={i} key={i.label} />
+      ))}
     </ul>
   )
 }
---
+++ src/utils/index.js
@@ -0,0 +1,66 @@
+export const items = [
+  {
+    label: 'Những thứ cơ bản',
+    url: '',
+    submenu: [
+      {
+        label: 'Khởi nghiệp là gì',
+        url: '2PACX-1vQGgH5HglrtN4H6m1W_0Z35TNpdEBNuGxLaSY_CfN9fnFMV4XbUgVhCFzAxt6IEoAPetUgLX8ZbktZx',
+      },
+      {
+        label: 'Các giai đoạn khởi nghiệp',
+        url: '2PACX-1vTdE4TjDe2T9nu4TPGBf4Zz9uSbFewv0MzSLsW86PsOBWN1O0Ho8RrIKEhkv4VcGnJBF2qWDI25mU0k',
+      },
+      {
+        label: 'Tham gia một công ty khởi nghiệp',
+        url: '2PACX-1vS3KLyYbO5XMyRFiQNGtztww7Qp8LggbjrCEiMex6h2oxT2XxDrkiUK7Wk3gJqx3k3R4JYuEIiZyi_h',
+      },
+      {
+        label: 'Tại sao nên làm việc tại một công ty khởi nghiệp',
+        url: '2PACX-1vR2-lurxR7npZ_qKp3ZjwLeNH4Pxemq9QRfUxfkoxiTyOL5g2BQbuZJaLeeLCafnO13N8mahXVYRTn6',
+      },
+      {
+        label: 'Tại sao không nên làm việc tại một công ty khởi nghiệp',
+        url: '2PACX-1vQClV-Ohy0oBTqL9lfsMVppUc8G1PvqWMMjsPnHsuakjUL5-XK8yS0SwKXZkXgADxBR-wJU-dRHhBb6',
+      },
+      {
+        label: 'Viết một sơ yếu lý lịch tuyệt vời',
+        url: '2PACX-1vQoOnwOMVGkMZ21cOvNwZUcnVJ8tMPvsG1oNZq3S4JCgvBVsMWwP8t6AkniFAb8HvUc49UJ4ccFmsB8',
+      },
+    ]
+  },
+  {
+    label: 'Lời khuyên khởi nghiệp',
+    url: '',
+    submenu: [
+      {
+        label: 'Tạo ra thứ gì đó mà mọi người yêu thích',
+        url: '2PACX-1vSbtgVLsGMWZlnYt2syAmFLeSSLzXes-4aJOZnYxrYCW8p19-XCLwbUYulCCBAjm1tcbVUGqB27SOEZ',
+      },
+      {
+        label: 'Làm những việc không mở rộng quy mô',
+        url: '2PACX-1vQQnr_xn8doZtiZnFLkk-kStIbQLu7EXylC5_eZRFo2LA4Y-d-nIn_6jxGsgyATElv6_9UJDTDQiBhR',
+      },
+      {
+        label: 'Suy thoái kinh tế',
+        url: '2PACX-1vQIyPlYTQdwGxWh-G96_LkhVKeibe9VwJq8XG2Dn27CahLNL-iRqX-uJSCa6K0dTnyDtrW7Ac-O31m3',
+      },
+      {
+        label: 'Không ngừng chăm chỉ',
+        url: '2PACX-1vTwpqNR2nI12ULj9jzHfKeMfaRwuXhD8aZZ-PGSo0Bt6V0dJjDrrgjK1_PmgYeh0d-wafHL-jfCsftX',
+      },
+      {
+        label: 'Công ty khởi nghiệp phát triển nhanh chóng',
+        url: '2PACX-1vQKlGhuwHQlZ5zT41IzE4SF7SUFDseY5zPLbXC2CFLbPLtYvv6im4IBNHJ74La22ZPJwlcOk-Cx7o45',
+      },
+      {
+        label: 'Khởi nghiệp = Tăng trưởng',
+        url: '2PACX-1vTjrPn6szVpKKNloxqG8OnQokG7i1bMzUDmQz82VbY6B3QJY1bMNwfC0MGy8TeNfYyKuq0sh6KvaDL4',
+      },
+      {
+        label: '90/10 Giải pháp',
+        url: '2PACX-1vT3xcjBHOd0UuUPAT00ALl8-51KeWOWHRrOtYnalsXgtwnD3Yt5DQFKhKY8yV6pP44OcFOSt9NqZqve',
+      },
+    ]
+  }
+]
